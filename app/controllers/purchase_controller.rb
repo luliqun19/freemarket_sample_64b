e0083fcfd3ebc33536f2c6f7dd8223823827dbc7
luliqun19 <luliqun19@yahoo.co.jp>
2020-04-04 03:11:53 +0900

    商品購入機能0404
    ●add purchase controller
    ●add purchased view
    ●change routes
    ●delete items/id/edit view

---
+++ app/controllers/purchase_controller.rb
@@ -0,0 +1,10 @@
+class PurchaseController < ApplicationController
+ 
+  def index
+    # user item 出品 機能 ss 設定待ち
+     end
+  
+  def pay
+    # card sever side waiting
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   root "items#index"
-  resources :items, only: [:index, :show , :edit, :new, :create]
+  resources :items, only: [:index, :show, :new, :create] do
+    resources :purchase, only: [:index, :create] do
+      collection do
+        post 'pay'
+        get 'purchased'
+      end
+    end
+  end
   resources :images, only: [:index]
   resources :users, only: [:new]
   resources :addresses, only: [:new, :create, :show, :edit, :update]
